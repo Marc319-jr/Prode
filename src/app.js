c155e88acadca1f014b72a05879036dfc240a3e3
Marc319-jr <marcelo.sznek@gmail.com>
2021-06-04 17:57:02 -0300

    comienzo de proyecto, Diseño de raiz

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
+
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+//URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
+app.use(express.urlencoded({ extended: false }));
+
+//motor de EJS
+app.set('view engine', 'ejs');
+
+
+//rutas
+const indexRouter = require('./routes/index');
+
+
+app.use('/' , indexRouter)
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto ' + PORT)
+}
+
+);
---
+++ src/controller/index.js
@@ -0,0 +1,9 @@
+const controller = { 
+
+    index: (req,res) => {
+        res.render('../src/views/index');
+    }
+}
+
+
+module.exports = controller;
