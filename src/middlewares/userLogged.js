ad52557312a4a7a817f80ca97887abade2d85218
Marc319-jr <marcelo.sznek@gmail.com>
2021-06-16 14:51:47 -0300

    index con datos de prode y de usuarios

--- src/middlewares/userLogged.js
+++ src/middlewares/userLogged.js
@@ -1,8 +1,11 @@
 const User = require('../model/User')
-
+const Prode = require('../model/Prode')
 
 function userLoggedMiddleWare(req,res,next){
     console.log("Estoy en el middleware de Userlogged");
+    let allProdes = Prode.findAll();
+    let prode = allProdes[0];
+    res.locals.prode = prode;
     if(req.session.userLogged)
     {
         console.log("Hay un usuario logeado");
