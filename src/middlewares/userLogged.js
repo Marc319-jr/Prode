08f2cdabe8137b9b8282c54adf64d0e9a730a8f8
PedroKahane <pedrok@datosclaros.com>
2021-06-17 16:15:13 -0300

    intento solucionar heroku

--- src/middlewares/userLogged.js
+++ src/middlewares/userLogged.js
@@ -1,7 +1,7 @@
 //const User = require('../model/User')
 const Prode = require('../model/Prode')
 
-/*function userLoggedMiddleWare(req,res,next){
+function userLoggedMiddleWare(req,res,next){
     console.log("Estoy en el middleware de Userlogged");
     let allProdes = Prode.findAll();
     let prode = allProdes[0];
@@ -11,14 +11,14 @@ const Prode = require('../model/Prode')
         console.log("Hay un usuario logeado");
         res.locals.isLogged = true;
         res.locals.userLogged = req.session.userLogged
-    } else {
+    } /*else {
         console.log("nadie esta logeado");
     };
 
     next();
-
-};
 */
+};
+
 
 
 module.exports = userLoggedMiddleWare
