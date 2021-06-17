639a546782547105b1bd983691bab4664faae398
PedroKahane <pedrok@datosclaros.com>
2021-06-17 18:23:23 -0300

    test.js

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 const { json } = require("express");
+const Prode = require("./src/model/Prode");
+const User = require("./src/model/User");
+const allProdes = Prode.findAll()
+const allUser = User.findAll()
 const Jugado = {
-"resultado": [
-        "4",
-        "4"
-    ]
+    resultado : allProdes[0].grupos[0].partidos[0].resultado
 };
 const Pedro = {
-    "puntos" : 0,
-    "resultado": [
-        "4",
-        "1"
-    ]
+    puntos : allUser[0].puntos,
+    resultado: allUser[0].grupos[0].partidos[0].resultado
 };
+
+
 const resultadoPartido = function(resultadoA,resultadoB) {
     let resultado = resultadoA - resultadoB;
     let stringResultado;
@@ -28,15 +28,16 @@ const resultadoPartido = function(resultadoA,resultadoB) {
 const sumaPuntos = function(comoSalio, Prode, persona, Partido) {
     if(comoSalio == Prode) {
         if(persona.resultado[0] == Partido.resultado[0] && persona.resultado[1] == Partido.resultado[1]){
-            persona.puntos = persona.puntos + 3
+            persona.puntos = (parseFloat(persona.puntos) + 3)
         } else {
-            persona.puntos = persona.puntos + 1
+            persona.puntos = (parseFloat(persona.puntos) + 1)
         }
     }else {
         persona.puntos = persona.puntos
     }
     return persona.puntos
 }
+
 console.log(sumaPuntos(resultadoPartido(Jugado.resultado[0],Jugado.resultado[1]), resultadoPartido(Pedro.resultado[0],Pedro.resultado[1]),Pedro,Jugado))
 console.log(resultadoPartido(Pedro.resultado[0],Pedro.resultado[1]))
 console.log(resultadoPartido(Jugado.resultado[0],Jugado.resultado[1]))
