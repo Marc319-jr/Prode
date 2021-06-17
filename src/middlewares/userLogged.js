e7e382e614f2846f1a887dfafc0938bcc93df607
PedroKahane <pedrok@datosclaros.com>
2021-06-17 16:19:35 -0300

    intento solucionar heroku

--- src/middlewares/userLogged.js
+++ src/middlewares/userLogged.js
@@ -8,13 +8,13 @@ function userLoggedMiddleWare(req,res,next){
     res.locals.prode = prode;
     if(req.session.userLogged)
     {
-        /*console.log("Hay un usuario logeado");*/
-        res.locals.isLogged = true;
-        res.locals.userLogged = req.session.userLogged
-    } /*else {
+        console.log("Hay un usuario logeado");
+        /*res.locals.isLogged = true;
+        res.locals.userLogged = req.session.userLogged*/
+    }  else {
         console.log("nadie esta logeado");
     };
-*/
+
     next();
 
 };
