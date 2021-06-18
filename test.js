3f486b0bb26a1a20151d9513ad7fc1cd337708db
PedroKahane <pedrok@datosclaros.com>
2021-06-18 11:45:44 -0300

    termine modelito de puntos

--- test.js
+++ test.js
@@ -23,32 +23,35 @@ const resultadoPartido = function(resultadoA,resultadoB) {
 }
 
 const sumaPuntos = function(comoSalio, Prode, persona, Partido) {
+    puntos = 0
     if(comoSalio == Prode) {
         if(persona.resultado[0] == Partido.resultado[0] && persona.resultado[1] == Partido.resultado[1]){
-            persona.puntos = (parseFloat(persona.puntos) + 3)
+            puntos = (parseFloat(puntos) + 3)
         } else {
-            persona.puntos = (parseFloat(persona.puntos) + 1)
+            puntos = (parseFloat(puntos) + 1)
         }
     }else {
-        persona.puntos = persona.puntos
+        puntos = puntos
     }
-    return persona.puntos
+    return parseFloat(puntos)
 }
 const puntosPorPersona = function() {
     for (let p = 0; p < allUser.length; p++) {
+        puntos = 0
         for (let i = 0; i < grupos.length; i++) {
             for (let j = 0; j < partidos.length; j++) {
                 if(allProdes[0].grupos[i].partidos[j].resultado[0] != "x" && allProdes[0].grupos[i].partidos[j].resultado[1] != "x"){
-                    sumaPuntos(resultadoPartido(allUser[p].grupos[i].partidos[j].resultado[0],allUser[p].grupos[i].partidos[j].resultado[1]), resultadoPartido(allProdes[0].grupos[i].partidos[j].resultado[0],allProdes[0].grupos[i].partidos[j].resultado[1]),allUser[p].grupos[i].partidos[j],allProdes[0].grupos[i].partidos[j])
+                    puntos = puntos + sumaPuntos(resultadoPartido(allUser[p].grupos[i].partidos[j].resultado[0],allUser[p].grupos[i].partidos[j].resultado[1]), resultadoPartido(allProdes[0].grupos[i].partidos[j].resultado[0],allProdes[0].grupos[i].partidos[j].resultado[1]),allUser[p].grupos[i].partidos[j],allProdes[0].grupos[i].partidos[j])
             
                 }
             }
         }
-        console.log(allUser[p].username)  
-        console.log(allUser[p].puntos)
+        console.log(allUser[p].username) 
+        console.log(puntos)
     }    
 }
-//console.log(sumaPuntos(resultadoPartido(Jugado.resultado[0],Jugado.resultado[1]), resultadoPartido(Pedro.resultado[0],Pedro.resultado[1]),Pedro,Jugado))
+
 //console.log(resultadoPartido(Pedro.resultado[0],Pedro.resultado[1]))
 //console.log(resultadoPartido(Jugado.resultado[0],Jugado.resultado[1]))
 puntosPorPersona()
+
