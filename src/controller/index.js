8c2257ac379a20ce31335c490807dfe90a4667fe
Marc319-jr <marcelo.sznek@gmail.com>
2021-06-21 00:20:46 -0300

    te agregue el array de usuarios en el INDEX

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,12 @@
 const Prode = require('../model/Prode')
+const User = require('../model/User');
 const controller = { 
 
     index: (req,res) => {
         let prodes = Prode.findAll();
+        let users = User.findAll()
         let prode  = prodes[0];
-        res.render('../src/views/index' , {'prode' : prode});
+        res.render('../src/views/index' , {'prode' : prode , 'users': users});
     }
 }
 
