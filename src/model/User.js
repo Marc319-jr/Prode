b3471fcadeb5a9603c2f04f5684a6f378ab7275a
PedroKahane <pedrok@datosclaros.com>
2021-06-23 23:09:34 -0300

    agregue resultados

--- src/model/User.js
+++ src/model/User.js
@@ -196,14 +196,6 @@ const User ={
                 {
                     return -1
                 }
-                if(a.plenos < b.plenos)
-                {
-                    return 1;
-                }
-                else if( a.plenos > b.plenos)
-                {
-                    return -1
-                }
                 else{
                     return 0
                 }
