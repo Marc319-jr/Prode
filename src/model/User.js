6f4e2278d1f41b6802786720ccd3ed1fb3eab1cf
PedroKahane <pedrok@datosclaros.com>
2021-06-23 21:43:28 -0300

    agregue a joaco

--- src/model/User.js
+++ src/model/User.js
@@ -196,6 +196,14 @@ const User ={
                 {
                     return -1
                 }
+                if(a.plenos < b.plenos)
+                {
+                    return 1;
+                }
+                else if( a.plenos > b.plenos)
+                {
+                    return -1
+                }
                 else{
                     return 0
                 }
